ec66f05d1fb5878ab890d9fe5e6d9de2fcd12365
Matt Bonness <mbonness@users.noreply.github.com>
2017-02-09 14:34:33 -0600

    Singleton
    Added Singleton class

---
+++ app/src/main/java/com/motorolasolutions/interviewapp/Singleton.java
@@ -0,0 +1,14 @@
+package com.motorolasolutions.interviewapp;
+
+/**
+ * Created by w20682 on 2/9/17.
+ */
+
+public class Singleton {
+    // TODO: member variables
+
+    public static Singleton getInstance() {
+        // TODO: implement
+        return new Singleton();
+    }
+}
